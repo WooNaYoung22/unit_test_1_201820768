81b1af7f9dc52ce7e022f3dc95833e0e18773665
nyw <skdud@ajou.ac.kr>
2020-07-20 19:11:46 +0900

    Update test

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -2,8 +2,11 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
 
 public class CustomCalculatorTest {
 
@@ -15,6 +18,8 @@ public class CustomCalculatorTest {
         customCalculator = new CustomCalculator();
         int result = customCalculator.add(10,15);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result == 25);
+        assertThat(result, is(25));
         System.out.println("result :: " + result);
     }
 
@@ -24,7 +29,17 @@ public class CustomCalculatorTest {
         customCalculator = new CustomCalculator();
         int result = customCalculator.subtract(23,10);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result == 13);
+        assertFalse(result == 12);
+        assertThat(result, is(13));
         System.out.println("result :: " + result);
+    }
+
+    @Test
+    public void 빼기테스트(){
+        customCalculator = new CustomCalculator();
+        int result = customCalculator.subtract(23,10);
+        assertFalse(result == 12);
     }
 
     //곱하기 테스트 작성
@@ -33,6 +48,8 @@ public class CustomCalculatorTest {
         customCalculator = new CustomCalculator();
         int result = customCalculator.multiply(5,9);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertThat(result, is(45));
+        assertTrue(result == 45);
         System.out.println("result :: " + result);
     }
 
@@ -42,6 +59,35 @@ public class CustomCalculatorTest {
         customCalculator = new CustomCalculator();
         int result = customCalculator.divide(25,5);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertThat(result, is(5));
         System.out.println("result :: " + result);
     }
+
+    @Test(timeout = 4000)
+    public void timeInMethodTest() throws InterruptedException {
+        Thread.sleep(5000);
+    }
+
+    @Test(timeout = 3000)
+    public void 시간제한체크() throws InterruptedException{
+        Thread.sleep(2000);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIsEmptyIndexOutOfBoundException(){
+        new ArrayList<Object>().get(0);
+    }
+
+    @Test
+    public void testMethod(){
+        ArrayList<Object> myList = new ArrayList<>();
+        assertThat(myList, is(empty()));
+    }
+
+    @Test
+    public void isEmptyArray() {
+        ArrayList<Object> myList = new ArrayList<>();
+        assertThat(myList, is(empty()));
+    }
+
 }
